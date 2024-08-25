a253a00aadf9b6cb5a9e471ecdab1443c1cfe69d
rhasanm <hasanrakibul.masum@gmail.com>
2024-08-25 12:20:37 +0600

    layout design

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,57 @@
+import { Layout } from '@/components/custom/layout'
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
+
+export default function Dashboard() {
+  return (
+    <Layout>
+      {/* ===== Top Heading ===== */}
+      <Layout.Header>
+        <div className='ml-auto flex items-center space-x-4'>
+        </div>
+      </Layout.Header>
+
+      {/* ===== Main ===== */}
+      <Layout.Body>
+        <div className='mb-2 flex items-center justify-between space-y-2'>
+          <h1 className='text-2xl font-bold tracking-tight'>Dashboard</h1>
+        </div>
+        <Tabs
+          orientation='vertical'
+          defaultValue='overview'
+          className='space-y-4'
+        >
+          <div className='w-full overflow-x-auto pb-2'>
+            <TabsList>
+              <TabsTrigger value='overview'>Overview</TabsTrigger>
+              <TabsTrigger value='reports'>Reports</TabsTrigger>
+              <TabsTrigger value='notifications'>Notifications</TabsTrigger>
+            </TabsList>
+          </div>
+        </Tabs>
+      </Layout.Body>
+    </Layout>
+  )
+}
+
+// const topNav = [
+//   {
+//     title: 'Overview',
+//     href: 'dashboard/overview',
+//     isActive: true,
+//   },
+//   {
+//     title: 'Customers',
+//     href: 'dashboard/customers',
+//     isActive: false,
+//   },
+//   {
+//     title: 'Products',
+//     href: 'dashboard/products',
+//     isActive: false,
+//   },
+//   {
+//     title: 'Settings',
+//     href: 'dashboard/settings',
+//     isActive: false,
+//   },
+// ]
---
+++ src/router.tsx
@@ -0,0 +1,90 @@
+import { createBrowserRouter } from 'react-router-dom'
+import GeneralError from './pages/errors/general-error'
+import NotFoundError from './pages/errors/not-found-error'
+import MaintenanceError from './pages/errors/maintenance-error'
+import UnauthorisedError from './pages/errors/unauthorised-error'
+
+const router = createBrowserRouter([
+  // Main routes
+  {
+    path: '/',
+    lazy: async () => {
+      const AppShell = await import('./components/app-shell')
+      return { Component: AppShell.default }
+    },
+    errorElement: <GeneralError />,
+    children: [
+      {
+        index: true,
+        lazy: async () => ({
+          Component: (await import('./pages/dashboard')).default,
+        }),
+      },
+      {
+        path: 'tasks',
+        lazy: async () => ({
+          Component: (await import('@/pages/tasks')).default,
+        }),
+      },
+      {
+        path: 'settings',
+        lazy: async () => ({
+          Component: (await import('./pages/settings')).default,
+        }),
+        errorElement: <GeneralError />,
+        children: [
+          {
+            index: true,
+            lazy: async () => ({
+              Component: (await import('./pages/settings/profile')).default,
+            }),
+          },
+          {
+            path: 'account',
+            lazy: async () => ({
+              Component: (await import('./pages/settings/account')).default,
+            }),
+          },
+          {
+            path: 'appearance',
+            lazy: async () => ({
+              Component: (await import('./pages/settings/appearance')).default,
+            }),
+          },
+          {
+            path: 'notifications',
+            lazy: async () => ({
+              Component: (await import('./pages/settings/notifications'))
+                .default,
+            }),
+          },
+          {
+            path: 'display',
+            lazy: async () => ({
+              Component: (await import('./pages/settings/display')).default,
+            }),
+          },
+          {
+            path: 'error-example',
+            lazy: async () => ({
+              Component: (await import('./pages/settings/error-example'))
+                .default,
+            }),
+            errorElement: <GeneralError className='h-[50svh]' minimal />,
+          },
+        ],
+      },
+    ],
+  },
+
+  // Error routes
+  { path: '/500', Component: GeneralError },
+  { path: '/404', Component: NotFoundError },
+  { path: '/503', Component: MaintenanceError },
+  { path: '/401', Component: UnauthorisedError },
+
+  // Fallback 404 route
+  { path: '*', Component: NotFoundError },
+])
+
+export default router
