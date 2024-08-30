65cb075442032cf5428734b6c12a34a1bfc0f7d7
Md. Rakibul Hasan <hasanrakibul.masum@gmail.com>
2024-08-31 02:17:59 +0600

    updated board column height on kanban

--- src/pages/tasks/components/BoardColumn.tsx
+++ src/pages/tasks/components/BoardColumn.tsx
@@ -57,7 +57,7 @@ export function BoardColumn({ column, tasks, isOverlay }: BoardColumnProps) {
   };
 
   const variants = cva(
-    "h-[500px] max-h-[500px] w-[350px] max-w-full bg-primary-foreground flex flex-col flex-shrink-0 snap-center",
+    "h-[650px] max-h-[650px] w-[350px] max-w-full bg-primary-foreground flex flex-col flex-shrink-0 snap-center",
     {
       variants: {
         dragging: {
